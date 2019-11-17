f06f7bde99d361929e8aae1b0a17473161f68c73
Eugene ZA <oigenZ@icloud.com>
2019-11-17 16:17:29 +0200

    added getEntity and getEntityList implementations

--- src/main/java/com/lab2/dao/DAOImpl.java
+++ src/main/java/com/lab2/dao/DAOImpl.java
@@ -1,17 +1,21 @@
 package com.lab2.dao;
 
+import com.lab2.model.DiscriminationColumn;
+import com.lab2.model.DiscriminatorValue;
+import com.lab2.model.TableName;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class DAOImpl<T> implements IDAOImpl<T> {
 
-    Class<T> clazz;
-    Connection connection;
-
-    PreparedStatement statement;
+    private Class<T> clazz;
+    private Connection connection;
+    private Exception ex;
 
     public DAOImpl(Class<T> clazz, Connection connection) {
         this.clazz = clazz;
@@ -19,43 +23,36 @@ public class DAOImpl<T> implements IDAOImpl<T> {
     }
 
     @Override
-    public T getEntity(Long id) {
+    public T getEntity(Long id) throws SQLException {
+        T entity;
+        TableName tableAnnotation = clazz.getAnnotation(TableName.class);
 
-//        T obj = clazz.newInstance();
-//
-//        String name = "Student 1";
-//        Integer age = 20;
-//        clazz.getName(); // name of table
-//        Field[] fields = clazz.getFields();
-//
-//        fields[0].getName(); // info about columns
-//
-//        String nameSetter = "setName";
-//
-//        Method m;
-//        try {
-//            m = clazz.getMethod(nameSetter, String.class);
-//        } catch (NoSuchMethodException e) {
-//            e.printStackTrace();
-//        }
-//
-//        m.invoke(obj, name);
-//
-//        try {
-//            T entity = clazz.newInstance();
-//        } catch (InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
+        String sql = String.format("SELECT * FROM public.%s WHERE id = ?", tableAnnotation.name());
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY
+        );
+        preparedStatement.setLong(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
 
+        try {
+            //@TODO map result set to list of objects
+        } catch (IndexOutOfBoundsException ex) {
+            entity = null;
+        }
 
         return null;
     }
 
     @Override
-    public List<T> getEntityList() {
-        return null;
+    public List<T> getEntityList() throws SQLException {
+        TableName tableAnnotation = clazz.getAnnotation(TableName.class);
+
+        String sql = String.format("SELECT * FROM public.%s", tableAnnotation.name());
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        return null; //@TODO map result set to list of objects
     }
 
 }
--- src/main/java/com/lab2/dao/IDAOImpl.java
+++ src/main/java/com/lab2/dao/IDAOImpl.java
@@ -1,8 +1,9 @@
 package com.lab2.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IDAOImpl<T> {
-    T getEntity(Long id);
-    List<T> getEntityList();
+    T getEntity(Long id) throws SQLException;
+    List<T> getEntityList()throws SQLException;
 }
