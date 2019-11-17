c852e1728082f68864ab7c46c9605320f407990b
Eugene ZA <oigenZ@icloud.com>
2019-11-17 21:06:15 +0200

    Finished DAOImpl

--- src/main/java/com/lab2/dao/DAOImpl.java
+++ src/main/java/com/lab2/dao/DAOImpl.java
@@ -5,7 +5,6 @@ import com.lab2.model.DiscriminatorValue;
 import com.lab2.model.TableName;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +14,61 @@ public class DAOImpl<T> implements IDAOImpl<T> {
 
     private Class<T> clazz;
     private Connection connection;
-    private Exception ex;
 
     public DAOImpl(Class<T> clazz, Connection connection) {
         this.clazz = clazz;
         this.connection = connection;
+    }
+
+    private List<Field> getAllFields(List<Field> fields, Class<?> type) {
+        fields.addAll(Arrays.asList(type.getDeclaredFields()));
+
+        if (type.getSuperclass() != null) {
+            getAllFields(fields, type.getSuperclass());
+        }
+
+        return fields;
+    }
+
+    private T createEntity(ResultSet resultSet, List<Field> fields) {
+        T entity;
+        try {
+            entity = clazz.getConstructor().newInstance();
+            for (Field field: fields) {
+                String name = field.getName();
+                String value = resultSet.getString(name);
+                Class type = field.getType();
+                field.set(entity, type.isEnum()
+                        ? type.getDeclaredMethod("fromString", String.class).invoke(null, value)
+                        : type.getConstructor(String.class).newInstance(value));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = null;
+        }
+        return entity;
+    }
+
+    private List<T> resultSetToList(ResultSet resultSet) throws SQLException {
+        DiscriminationColumn columnAnnotation = clazz.getAnnotation(DiscriminationColumn.class);
+        String discriminatorColumn = columnAnnotation == null ? null : columnAnnotation.name();
+        DiscriminatorValue discriminatorAnnotation = clazz.getAnnotation(DiscriminatorValue.class);
+        String discriminator = discriminatorAnnotation == null ? null : discriminatorAnnotation.value();
+
+        List<Field> fields = new ArrayList<>();
+        getAllFields(fields, clazz);
+
+        fields.forEach(field -> field.setAccessible(true));
+
+        List<T> list = new ArrayList<>();
+        resultSet.beforeFirst();
+        while (resultSet.next()) {
+            if(discriminatorColumn == null || resultSet.getString(discriminatorColumn).equals(discriminator)) {
+                T entity = createEntity(resultSet, fields);
+                list.add(entity);
+            }
+        }
+        return list;
     }
 
     @Override
@@ -35,12 +84,12 @@ public class DAOImpl<T> implements IDAOImpl<T> {
         ResultSet resultSet = preparedStatement.executeQuery();
 
         try {
-            //@TODO map result set to list of objects
+            entity = resultSetToList(resultSet).get(0);
         } catch (IndexOutOfBoundsException ex) {
             entity = null;
         }
 
-        return null;
+        return entity;
     }
 
     @Override
@@ -52,7 +101,7 @@ public class DAOImpl<T> implements IDAOImpl<T> {
                 sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        return null; //@TODO map result set to list of objects
+        return resultSetToList(resultSet);
     }
 
 }
