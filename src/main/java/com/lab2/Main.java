ea12d9fa61533beed128bd9ea650c1cc5fa6b11e
Eugene ZA <oigenZ@icloud.com>
2019-11-23 00:34:47 +0200

    Changed logs

--- src/main/java/com/lab2/Main.java
+++ src/main/java/com/lab2/Main.java
@@ -10,22 +10,19 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Properties props = new Properties();
-        Logger lgr = Logger.getLogger(Main.class.getName());
 
         try {
             FileInputStream in = new FileInputStream(System.getProperty("user.dir") + "/db.properties");
             props.load(in);
             in.close();
         } catch (IOException ex) {
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            System.out.println(ex.getMessage());
             return;
         }
 
@@ -37,19 +34,19 @@ public class Main {
             DAO dao = new DAO();
             dao.connect(url, user, password);
 
-            System.out.println("Students: ");
+            System.out.println("====> Students: ");
             List<Student> students = dao.getStudentList();
-            students.forEach(student -> System.out.println(student.getName()));
+            students.forEach(student -> System.out.println(student.getId().toString() + ") " + student.getName()));
 
-            System.out.println("Teachers: ");
+            System.out.println("====> Teachers: ");
             List<Teacher> teachers = dao.getTeacherList();
-            teachers.forEach(teacher ->  System.out.println(teacher.getName()));
+            teachers.forEach(teacher ->  System.out.println(teacher.getId().toString() + ") " + teacher.getName()));
 
-            System.out.println("Marks: ");
+            System.out.println("====> Marks: ");
             List<Mark> marks = dao.getMarkList();
-            marks.forEach(mark -> System.out.println(mark.getValue()));
+            marks.forEach(mark -> System.out.println(mark.getId().toString() + ") " + mark.getValue().toString()));
         } catch (SQLException ex) {
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            System.out.println(ex.getMessage());
         }
     }
 }
